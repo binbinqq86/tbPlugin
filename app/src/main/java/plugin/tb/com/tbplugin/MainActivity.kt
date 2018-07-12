70c6f8e9a36cabc0d47c54cef9bc656eabc42c46
田彬 <624715451@qq.com>
2018-07-12 16:26:48 +0800

    update

---
+++ app/src/main/java/plugin/tb/com/tbplugin/MainActivity.kt
@@ -0,0 +1,17 @@
+package plugin.tb.com.tbplugin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv.text = """ENVIRONMENT:${BuildConfig.ENVIRONMENT}
+app_name:${getString(R.string.app_name)}
+flavor:${BuildConfig.FLAVOR}
+                  """
+    }
+}
